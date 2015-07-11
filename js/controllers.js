307a5bb1fc9b3c2cbfdc9eb705dcd672883c6cb2
Scott Warner <scottbwarner@gmail.com>
2015-07-11 10:39:14 -0400

    add angular stat controller scope

--- js/controllers.js
+++ js/controllers.js
@@ -1,2 +1,14 @@
 var myApp = angular.module('myApp', []);
 
+myApp.controller('statController', function statController($scope){
+
+	$scope.stats = {
+		'str' : 8,
+		'dex' : 8,
+		'con' : 8,
+		'int' : 8,
+		'wis' : 8,
+		'cha' : 8,
+	};
+
+});
