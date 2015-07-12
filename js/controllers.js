f52141492e6296ae0a6082bbb599845463b4ebaf
Scott Warner <scottbwarner@gmail.com>
2015-07-12 09:56:09 -0400

    add ng-repeat

--- js/controllers.js
+++ js/controllers.js
@@ -2,25 +2,25 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('statController', function statController($scope) {
 
-        $scope.stats = [{
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'dex',
-                'value': 8
-            }, {
-                'label': 'con',
-                'value': 8
-            }, {
-                'label': 'int',
-                'value': 8
-            }, {
-                'label': 'wis',
-                'value': 8
-            }, {
-                'label': 'cha',
-                'value': 8
-            }];
+    $scope.stats = [{
+        'label': 'str',
+        'value': 8
+    }, {
+        'label': 'dex',
+        'value': 8
+    }, {
+        'label': 'con',
+        'value': 8
+    }, {
+        'label': 'int',
+        'value': 8
+    }, {
+        'label': 'wis',
+        'value': 8
+    }, {
+        'label': 'cha',
+        'value': 8
+    }, ];
 
     $scope.total = 27;
 
@@ -37,49 +37,49 @@ myApp.controller('statController', function statController($scope) {
 
     $scope.revertStats = function() {
         $scope.stats = [{
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'str',
-                'value': 8
-            }, {
-                'label': 'str',
-                'value': 8
-            }];
-
-
-    $scope.selectedRace = function(selected) {
-        console.log(selected);
-        $scope.race = selected;
-        $scope.revertStats();
-        switch ($scope.race.type) {
-            case "Human":
-                $scope.stats.str++;
-                $scope.stats.dex++;
-                $scope.stats.con++;
-                $scope.stats.int++;
-                $scope.stats.wis++;
-                $scope.stats.cha++;
-                break;
-            case "Hill Dwarf":
-                $scope.stats.con += 2;
-                $scope.stats.wis++;
-        }
+            'label': 'str',
+            'value': 8
+        }, {
+            'label': 'dex',
+            'value': 8
+        }, {
+            'label': 'con',
+            'value': 8
+        }, {
+            'label': 'int',
+            'value': 8
+        }, {
+            'label': 'wis',
+            'value': 8
+        }, {
+            'label': 'cha',
+            'value': 8
+        }, ];
     };
 
-    $scope.plusStr = function() {
-        $scope.stats.str++;
-        $scope.total--;
-    };
+    // $scope.selectedRace = function(selected) {
+    //     console.log(selected);
+    //     $scope.race = selected;
+    //     $scope.revertStats();
+    //     switch ($scope.race.type) {
+    //         case "Human":
+    //             $scope.stats.str++;
+    //             $scope.stats.dex++;
+    //             $scope.stats.con++;
+    //             $scope.stats.int++;
+    //             $scope.stats.wis++;
+    //             $scope.stats.cha++;
+    //             break;
+    //         case "Hill Dwarf":
+    //             $scope.stats.con += 2;
+    //             $scope.stats.wis++;
+    //     }
+    // };
+
+    // $scope.plusStr = function() {
+    //     $scope.stats.str++;
+    //     $scope.total--;
+    // };
 
 
 });
