73f5092c889fbd8ddbe813ede0a553c4c27b9a9d
Scott Warner <scottbwarner@gmail.com>
2015-09-13 16:40:03 -0400

    ready for bootstrap

--- js/behavior.js
+++ js/behavior.js
@@ -1,79 +1,79 @@
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	//Custom Modal Stuff
-	MODAL_OPEN = false;
+// 	//Custom Modal Stuff
+// 	MODAL_OPEN = false;
 
-	$('.displayArea').click(function() {
-      $('.intro').addClass('animated fadeOut');
-      window.setTimeout(function(){
-         $('.intro').css('display', 'none');
-      },200);
-		if(MODAL_OPEN === false){
-			window.location = ('#openModal');
-		MODAL_OPEN = true;
-		$('body').addClass('dark');
-		}
-		else if(MODAL_OPEN === true){
-			window.location = ('#close');
-			MODAL_OPEN = false;
-			$('body').removeClass('dark');
-		}
-	});
+	// $('.displayArea').click(function() {
+ //      // $('.intro').addClass('animated fadeOut');
+ //      // window.setTimeout(function(){
+ //      //    $('.intro').css('display', 'none');
+ //      // },200);
+	// 	if(MODAL_OPEN === false){
+	// 		window.location = ('#openModal');
+	// 	MODAL_OPEN = true;
+	// 	$('body').addClass('dark');
+	// 	}
+	// 	else if(MODAL_OPEN === true){
+	// 		window.location = ('#close');
+	// 		MODAL_OPEN = false;
+	// 		$('body').removeClass('dark');
+	// 	}
+	// });
     
 
-   //Custom Tab Stuff
+//    //Custom Tab Stuff
 
-   $('#skillsTab').click(function(){
-   	$('#statsTab').removeClass('active');
-   	$('#weaponsTab').removeClass('active');
-      $('#featuresTab').removeClass('active');
-   	$('#skillsTab').addClass('active');
+//    $('#skillsTab').click(function(){
+//    	$('#statsTab').removeClass('active');
+//    	$('#weaponsTab').removeClass('active');
+//       $('#featuresTab').removeClass('active');
+//    	$('#skillsTab').addClass('active');
 
-   	$('#tab1').removeClass('active');
-   	$('#tab3').removeClass('active');
-      $('#tab4').removeClass('active');
-   	$('#tab2').addClass('active');
-
-
-   });
-
-    $('#statsTab').click(function(){
-   	$('#skillsTab').removeClass('active');
-   	$('#weaponsTab').removeClass('active');
-      $('#featuresTab').removeClass('active');
-   	$('#statsTab').addClass('active');
-
-   	$('#tab2').removeClass('active');
-   	$('#tab3').removeClass('active');
-      $('#tab4').removeClass('active');
-   	$('#tab1').addClass('active');
-   });
-
-    $('#weaponsTab').click(function(){
-   	$('#skillsTab').removeClass('active');
-   	$('#statsTab').removeClass('active');
-      $('#featuresTab').removeClass('active');
-   	$('#weaponsTab').addClass('active');
-
-   	$('#tab2').removeClass('active');
-   	$('#tab1').removeClass('active');
-      $('#tab4').removeClass('active');
-   	$('#tab3').addClass('active');
-   });
-
-     $('#featuresTab').click(function(){
-      $('#skillsTab').removeClass('active');
-      $('#statsTab').removeClass('active');
-      $('#weaponsTab').removeClass('active');
-      $('#featuresTab').addClass('active');
-
-      $('#tab2').removeClass('active');
-      $('#tab1').removeClass('active');
-      $('#tab3').removeClass('active');
-      $('#tab4').addClass('active');
-   });
+//    	$('#tab1').removeClass('active');
+//    	$('#tab3').removeClass('active');
+//       $('#tab4').removeClass('active');
+//    	$('#tab2').addClass('active');
 
 
+//    });
 
-}); 
+//     $('#statsTab').click(function(){
+//    	$('#skillsTab').removeClass('active');
+//    	$('#weaponsTab').removeClass('active');
+//       $('#featuresTab').removeClass('active');
+//    	$('#statsTab').addClass('active');
+
+//    	$('#tab2').removeClass('active');
+//    	$('#tab3').removeClass('active');
+//       $('#tab4').removeClass('active');
+//    	$('#tab1').addClass('active');
+//    });
+
+//     $('#weaponsTab').click(function(){
+//    	$('#skillsTab').removeClass('active');
+//    	$('#statsTab').removeClass('active');
+//       $('#featuresTab').removeClass('active');
+//    	$('#weaponsTab').addClass('active');
+
+//    	$('#tab2').removeClass('active');
+//    	$('#tab1').removeClass('active');
+//       $('#tab4').removeClass('active');
+//    	$('#tab3').addClass('active');
+//    });
+
+//      $('#featuresTab').click(function(){
+//       $('#skillsTab').removeClass('active');
+//       $('#statsTab').removeClass('active');
+//       $('#weaponsTab').removeClass('active');
+//       $('#featuresTab').addClass('active');
+
+//       $('#tab2').removeClass('active');
+//       $('#tab1').removeClass('active');
+//       $('#tab3').removeClass('active');
+//       $('#tab4').addClass('active');
+//    });
+
+
+
+// }); 
